5545a2d3555757307297cf2b6e22484f87edc6d9
AbyHamza <hamzaigraet@gmail.com>
2023-03-15 18:32:52 +0300

    модуль B3.7 Циклы Елка

---
+++ yolka.py
@@ -0,0 +1,13 @@
+def yulka_ka(n):
+    for i in range(0, n):
+        for j in range(1, 5):
+            print((i + j) * '* ')
+
+yulka_ka(7)
+print()
+yulka_ka(3)
+print()
+yulka_ka(50)
+print()
+x = int(input())
+yulka_ka(x)
