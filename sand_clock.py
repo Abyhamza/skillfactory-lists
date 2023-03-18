37b80b6026670ebcdd0c7c9b217b63cabef020c4
AbyHamza <hamzaigraet@gmail.com>
2023-03-18 18:32:02 +0300

    модуль B3.7 Циклы Часы

--- sand_clock.py
+++ sand_clock.py
@@ -1,14 +1,27 @@
-def clock(n):
-    for i in range(1, n)[::-1]:
-        print(i * '* ')
-    for i in range(2, n):
-        print(i * '* ')
+# def clock(n):
+#     for i in range(1, n)[::-1]:
+#         print(i * '* ')
+#     for i in range(2, n):
+#         print(i * '* ')
+#
+# clock(5)
+#
+# def fantesi(n):
+#     for i in range(0, n):
+#         if i >= 0:
+#             print('# ', end= '')
+#         else:
+#             print(' *')
+#         for j in range(0, n):
+#             print('* ', end= ' ')
+#         print()
+#
+# fantesi(3)
 
-clock(5)
+genre = ['поп', 'рок', 'джаз']
 
-def fantesi(n):
-    for i in range(0, n):
-        print(n * '* # ')
-        print(n * '# * ')
-
-fantesi(3)
+for i in range(len(genre)):
+    if i == 1:
+        print("Мне не нравиться рок")
+    else:
+        print("Мне нравится", genre[i])
